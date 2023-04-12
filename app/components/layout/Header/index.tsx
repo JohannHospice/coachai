b499b5d7b0408b7a25ce5c8c5a423f17ef0be70b
Johann Hospice <johannhospice.dev@gmail.com>
2023-04-13 00:58:29 +0200

    Improving style

--- app/components/layout/Header/index.tsx
+++ app/components/layout/Header/index.tsx
@@ -4,7 +4,7 @@ import { Container } from "../../common/Container";
 export function Header() {
   return (
     <div className="sticky top-0 z-10">
-      <div className="border-b-0 border-white/10 bg-transparent bg-opacity-75 py-6 relative after:absolute after:inset-0 after:backdrop-blur-sm after:blur after:-z-10">
+      <div className="border-b border-white/10 bg-transparent bg-opacity-75 py-6 relative after:absolute after:inset-0 after:backdrop-blur-sm after:blur after:-z-10">
         <Container>
           <Link to="/" className="text-xl font-semibold">
             CoachAI
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,6 @@ module.exports = {
         blob: "rotate 15s linear infinite, scale 10s cubic-bezier(0.46, 0.03, 0.52, 0.96) infinite",
       },
       keyframes: {
-        // blob: {
-        //   "0%": {
-        //     translate: "0 -5vh",
-        //     scale: "1 1.25",
-        //     rotate: "0deg",
-        //   },
-        //   "50%": {
-        //     translate: "0 5vh",
-        //     scale: ".75 1",
-        //     rotate: "180deg",
-        //   },
-        //   "100%": {
-        //     translate: "0 -5vh",
-        //     scale: "1 1.25",
-        //     rotate: "360deg",
-        //   },
-        // },
         rotate: {
           "0%": {
             rotate: "0deg",
